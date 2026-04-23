e82019377503d5749cf689e01158a99aa61aa4ec
Pintu <pintupatidar555@gmail.com>
2026-04-23 15:15:07 +0530

    fix https redirect loop issues

--- QuestionAPI/QuestionAPI/settings.py
+++ QuestionAPI/QuestionAPI/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
 
